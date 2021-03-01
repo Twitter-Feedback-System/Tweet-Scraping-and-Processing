7344108d9963365734d8dd7fa40af9565ce8794d
Aliabbas78692 <18ce005@charusat.edu.in>
2021-03-01 16:51:22 +0530

    Done with Tweet Cleaning

--- clean_sentiment.py
+++ clean_sentiment.py
@@ -4,6 +4,10 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import tweepy
 import re
 
+positive_tweets = 0
+negative_tweets = 0
+neutral_tweets = 0
+
 tweets = {}
 tweet_list = []
 regrex_pattern = re.compile(pattern = "["
@@ -13,7 +17,6 @@ regrex_pattern = re.compile(pattern = "["
         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                            "]+", flags = re.UNICODE)
 f = open('credential.json')
-f2 = open('new.txt','a')
 data = json.load(f)
 api_key = data['API_Key']
 api_secret_key = data['API_SecretKey'] 
@@ -25,11 +28,12 @@ auth.set_access_token(access_token,access_token_secret)
 auth.secure = True
 api = tweepy.API(auth,wait_on_rate_limit = True,wait_on_rate_limit_notify = True)
 
-searchQuery = input('Enter a keyword:- ')
+searchQuery = input('Enter a hash keyword:- ')
 tweetsToSearch = int(input('Enter the number of tweets:- '))
- 
+
+searchQuery = '#' + searchQuery
 i =1
-for tweet in tweepy.Cursor(api.home_timeline,screen_name = searchQuery).items(tweetsToSearch):
+for tweet in tweepy.Cursor(api.search,q = searchQuery,lang = "en").items(tweetsToSearch):
     dict_tweets = dict()
     tweets = tweet
     dict_tweets['text'] = tweets.text.encode('utf-8')
@@ -45,8 +49,25 @@ for a in tweet_list:
     clean_tweet = re.sub('\n',' ',clean_tweet)
     a['text'] = clean_tweet 
 
-for a in tweet_list:
-    print(str(i) + ')' + a['text'] + '\n')
+for analyze in tweet_list:
+    print(str(i) + ')' + analyze['text'] + '\n')
     i = i + 1
+    analysis = TextBlob(analyze['text'])
+    if(analysis.sentiment.polarity == 0):
+        print('Neutral' + '\n')
+        print(str(analysis.sentiment.polarity) + '\n')
+        neutral_tweets += 1
+    elif(analysis.sentiment.polarity < 0):
+        print('Negative' + '\n')
+        print(str(analysis.sentiment.polarity) + '\n')
+        negative_tweets += 1
+    elif(analysis.sentiment.polarity > 0):
+        print('Positive' + '\n')
+        print(str(analysis.sentiment.polarity) + '\n')
+        positive_tweets += 1
+        
+
+print('No of neutral tweets :- '+ str(neutral_tweets) + '\nNo of negative tweets :- '+ str(negative_tweets) + '\nNo of positive tweets :-' +str(positive_tweets))
+    
 # with open('new.json','w') as outfile:
 #     json.dump(tweet_list,outfile)
